4b3283f32cc8999be2989c8a97c338584b603348
M677F323 <115022648+M677F323@users.noreply.github.com>
2022-12-11 21:31:20 -0600

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,10 +3,5 @@
 public partial class App : Application
 {
 	public static PmViewModel vm;
-	public App()
-	{
-		InitializeComponent();
-		vm = new PmViewModel();
-		MainPage = new NavigationPage(new LoginPage());
-	}
+
 }
