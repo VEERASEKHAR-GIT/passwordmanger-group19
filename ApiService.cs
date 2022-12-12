341bfb8d062532aa1e25602f491242aa1fafe1ff
M677F323 <115022648+M677F323@users.noreply.github.com>
2022-12-11 21:36:29 -0600

    Update ApiService.cs

--- ApiService.cs
+++ ApiService.cs
@@ -16,7 +16,33 @@ namespace PasswodManager
 
         public List<PasswordItem> listOfItems { get; private set; }
 
+        public ApiService()
+        {
+            _client = new HttpClient();
+            _serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+        }
 
+        public async Task<List<PasswordItem>> get()
+        {
+            String uid = await SecureStorage.GetAsync("uid");
+            String apiUrl = $"https://pocketbase-abby.fly.dev/api/collections/secrets/records?filter=(userId='{uid}')";
+            listOfItems = new List<PasswordItem>();
+
+            Uri uri = new Uri(string.Format(apiUrl, string.Empty));
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    var c = JsonSerializer.Deserialize<PasswordApiModel>(content, _serializerOptions);
+                    listOfItems = c.items;
+                }
+            }
             catch (Exception ex)
             {
                 Console.WriteLine(@"\tERROR {0}", ex.Message);
@@ -45,31 +71,8 @@ namespace PasswodManager
             }
         }
 
-        public async Task<bool> createUser(string email, string pass)
-        {
-            Uri uri = new Uri(string.Format("https://pocketbase-abby.fly.dev/api/collections/users/records", string.Empty));
-
-            try
-            {
-                var loginModel = new LoginModel();
-                loginModel.email = email;
-                loginModel.password = pass;
-                loginModel.passwordConfirm = pass;
-                string json = JsonSerializer.Serialize<LoginModel>(loginModel, _serializerOptions);
-                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
-
-                HttpResponseMessage response = null;
-
-                response = await _client.PostAsync(uri, content);
-                return true;
-            }
-            catch (Exception ex)
-            {
-
-            }
-            return false;
-        }
-
+        
+        
 
         public async Task<String> loginUser(string email, string pass)
         {
