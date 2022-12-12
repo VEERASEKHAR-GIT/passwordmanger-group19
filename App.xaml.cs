0bde31c433d51954bbedefcb22226914bef790c3
dheerajreddybadduri <115022128+dheerajreddybadduri@users.noreply.github.com>
2022-12-11 21:05:46 -0600

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,9 +4,5 @@ public partial class App : Application
 {
 	public static PmViewModel vm;
 	public App()
-	{
-		InitializeComponent();
-		vm = new PmViewModel();
-		MainPage = new NavigationPage(new LoginPage());
-	}
+	
 }
