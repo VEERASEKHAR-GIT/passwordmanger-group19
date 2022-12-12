9914e36ad372e61183c1af94331b4350ecf17538
M677F323 <115022648+M677F323@users.noreply.github.com>
2022-12-11 21:28:47 -0600

    Update LoginPage.xaml.cs

--- LoginPage.xaml.cs
+++ LoginPage.xaml.cs
@@ -29,6 +29,15 @@ public partial class LoginPage : ContentPage
         var x = (Entry)sender;
         email = x.Text;
     }
+    private void Entry_TextChanged_1(object sender, TextChangedEventArgs e)
+    {
+        var x = (Entry)sender;
+        pass = x.Text;
+    }
 
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new RegisterPage());
+    }
 
 }
