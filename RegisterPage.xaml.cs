cf127fc10a7816a9b1d9d1556b54e80d78399de8
M677F323 <115022648+M677F323@users.noreply.github.com>
2022-12-11 21:11:10 -0600

    Update RegisterPage.xaml.cs

--- RegisterPage.xaml.cs
+++ RegisterPage.xaml.cs
@@ -33,7 +33,16 @@ public partial class RegisterPage : ContentPage
         var x = (Entry)sender;
         email = x.Text;
     }
+    private void Entry_TextChanged_1(object sender, TextChangedEventArgs e)
+    {
+        var x = (Entry)sender;
+        pass = x.Text;
+    }
 
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new RegisterPage());
+    }
 
 
 }
