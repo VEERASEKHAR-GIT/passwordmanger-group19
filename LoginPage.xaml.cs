c08f07581027f0c2977766126a30e75e4457df7a
M677F323 <115022648+M677F323@users.noreply.github.com>
2022-12-11 21:28:18 -0600

    Update LoginPage.xaml.cs

--- LoginPage.xaml.cs
+++ LoginPage.xaml.cs
@@ -9,7 +9,20 @@ public partial class LoginPage : ContentPage
         InitializeComponent();
     }
 
-    
+   private async void Button_Clicked(object sender, EventArgs e)
+    {
+        var pmService = new ApiService();
+        var uid = await pmService.loginUser(email, pass);
+        if (uid != null)
+        {
+            await SecureStorage.SetAsync("uid", uid);
+            await Navigation.PushAsync(new MainPage());
+        }
+        else
+        {
+            await DisplayAlert("Alert", "Login Error, Please try again", "OK");
+        }
+    } 
 
     private void Entry_TextChanged(object sender, TextChangedEventArgs e)
     {
