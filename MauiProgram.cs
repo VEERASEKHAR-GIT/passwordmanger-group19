67b8bba0b74ebdcde7985e3c50e569f70db8ed58
M677F323 <115022648+M677F323@users.noreply.github.com>
2022-12-11 21:26:22 -0600

    Update MauiProgram.cs

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -6,7 +6,12 @@ public static class MauiProgram
 	{
 		var builder = MauiApp.CreateBuilder();
 		builder
-
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
 
 		return builder.Build();
 	}
