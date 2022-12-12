308e730fbc55db68e29415b97c77df77bdc7a4f6
M677F323 <115022648+M677F323@users.noreply.github.com>
2022-12-11 21:24:26 -0600

    Update NewEntryPage.xaml.cs

--- NewEntryPage.xaml.cs
+++ NewEntryPage.xaml.cs
@@ -9,20 +9,5 @@ public partial class NewEntryPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void Entry_TextChanged(object sender, TextChangedEventArgs e)
-	{
-		appname = e.NewTextValue;
-	}
 
-	private void Entry_TextChanged_1(object sender, TextChangedEventArgs e)
-	{
-		password = e.NewTextValue;
-	}
-
-	private async void Button_Clicked(object sender, EventArgs e)
-	{
-		await App.vm.addNewPassword(appname, password);
-		await DisplayAlert("HUrray", "New Application password saved", "View all");
-		await Navigation.PopAsync();
-	}
 }
